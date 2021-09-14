022b5d3774513c0bf2d0414ded1f451c35649245
Teresa Atencio <teresa.atencio.robles@hotmail.com>
2021-09-14 18:38:29 -0500

    Formulario-Funcional-Con Cambio de plantillas

---
+++ Controllers/Adopcion.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using S7_PC2.Models;
+
+namespace S7_PC2.Controllers
+{
+    public class AdopcionController : Controller
+    {
+        private readonly ILogger<AdopcionController> _logger;
+
+        public AdopcionController(ILogger<AdopcionController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Adopcion()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Adopcion(Adopcion adopcion)
+        {
+            Console.WriteLine(adopcion.Nombres);
+            return RedirectToAction("AdopcionConfirmacion");
+        }
+
+        public IActionResult AdopcionConfirmacion()
+        {
+            return View();
+        }
+        
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Adopcion.cs
@@ -0,0 +1,14 @@
+namespace S7_PC2.Models
+{
+    public class Adopcion
+    {
+        public string Nombres { get; set; }
+        public string ApellidoPa { get; set; }
+        public string Tipo { get; set; }
+        public string ApellidoMa { get; set; }
+        public string Descripcion { get; set; }
+        public string Telefono { get; set; }
+        public string Distrito { get; set; }
+        public string Direccion { get; set; }
+    }
+}
