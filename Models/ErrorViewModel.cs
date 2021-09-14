07cb85bdc1511d17c7f8b6828d1f1ca0971e9c82
Teresa Atencio <teresa.atencio.robles@hotmail.com>
2021-09-14 17:11:55 -0500

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace S7_PC2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
